015b615a142b3fe00be6dea7885a79f72ca347b9
Luan Vieira <luanvwittchind@gmail.com>
2025-10-22 09:51:49 -0300

    Corrige exibição de postagens publicadas no template index.html

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def index(request):
-    post_list = Post.publicados.all().order_by('-criado_em')
-    context = {'post_list': post_list}
+    posts = Post.publicados.all().order_by('-criado_em')
+    context = {'posts': posts}
     return render(request, 'blog/index.html', context)
-
 
 def post_detail(request, slug):
     post = get_object_or_404(Post.publicados, slug=slug)
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
+
+
